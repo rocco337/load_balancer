373e73855c49c1e0c89a6d25947245e97bcfc8ec
roko bobic <roko.bobic@gmail.com>
2020-02-07 11:17:49 +0100

    read port from commandline

--- main.go
+++ main.go
@@ -1,20 +1,34 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"net/http"
 	"net/http/httputil"
 	"time"
 )
 
+var serverPort int
+
+func init() {
+	flag.IntVar(&serverPort, "p", 8090, "default loadbalancer port")
+}
+
+//auto discovery
+//configuration - port, autodiscovery
+//server health - remove from list if unhealthy
 func main() {
+	flag.Parse()
 
 	nodes := []string{"localhost:8082", "localhost:8081"}
 	new(RoundRobinStrategy).Instance().Init(nodes)
 
+	fmt.Println("Starting loadbalancer...")
+	fmt.Printf("Listening to port %v \n", serverPort)
+
 	//open proxy server that forwards request to one of active nodes
 	server := &http.Server{
-		Addr:           ":8090",
+		Addr:           fmt.Sprintf(":%v", serverPort),
 		Handler:        http.HandlerFunc(handler),
 		ReadTimeout:    10 * time.Second,
 		WriteTimeout:   10 * time.Second,
